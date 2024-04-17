49e20bf69e14618a637688f96980ef15c8ba4aea
Thiru-cse <thiru.c.cse@gmail.com>
2024-04-17 17:53:03 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// 1. Create console.log and print hello
+console.log("hello");
+// 2. Declare variable name with value "John Doe"
+let name = "John Doe";
+// 3. Log the content of the variable 'name' to the console using console.log
+console.log(name);
